3d9c3d92c5b20eb0848c08c113cb81c46a346420
ahmedskaram <ahmedkaramak47@gmail.com>
2024-11-26 09:14:05 -0800

    Advanced Model Queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,34 @@
+import django
+from django.conf import settings
 from relationship_app.models import Author, Book, Library, Librarian
 
-author = Author.objects.get(name='J.K. Rowling')
-books = author.books.all()
-for book in books:
-    print(book.title)
+# Set up Django settings (for running queries outside of the server)
+settings.configure(DEBUG=True, DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': 'db.sqlite3'}})
+django.setup()
 
-library = Library.objects.get(name='Central Library')
-books = library.books.all()
-for book in books:
-    print(book.title)
+# Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = author.books.all()
+    print(f'Books by {author_name}:')
+    for book in books:
+        print(book.title)
 
-library = Library.objects.get(name='Central Library')
-librarian = library.librarian
-print(librarian.name)
+# List all books in a specific library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    print(f'Books in {library_name} Library:')
+    for book in books:
+        print(book.title)
+
+# Retrieve the librarian for a specific library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    print(f'Librarian for {library_name} Library: {librarian.name}')
+
+# Example usage
+books_by_author('J.K. Rowling')
+books_in_library('Central Library')
+librarian_for_library('Central Library')
