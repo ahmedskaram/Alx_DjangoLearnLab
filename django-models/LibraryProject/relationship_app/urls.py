6d8d0d0c0e3de20808e9457834ed4cb48f11cfc0
ahmedskaram <ahmedkaramak47@gmail.com>
2024-11-27 04:23:52 -0800

    Add user authentication views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,8 +13,8 @@ from django.urls import path
 from .views import UserLoginView, UserLogoutView, register
 
 urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('login/', UserLoginView.as_v_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', UserLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', register, name='register'),
 ]
 
