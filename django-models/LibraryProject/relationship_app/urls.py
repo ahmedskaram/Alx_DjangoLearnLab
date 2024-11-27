ddbfdcb793c42c234e6886e51126ef816368be4c
ahmedskaram <ahmedkaramak47@gmail.com>
2024-11-27 04:19:03 -0800

    Add user authentication views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,5 +4,20 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
 ]
+
+# -----------------------------------------------------------------------------------------
+
+from django.urls import path
+from .views import UserLoginView, UserLogoutView, register
+
+urlpatterns = [
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+]
+
+# -----------------------------------------------------------------------------------------
+
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,8 @@ def list_books(request):
     books = Book.objects.select_related('author').all()  # This too
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+# -----------------------------------------------------------------------------------------
+
 from django.views.generic.detail import DetailView
 from .models import Library
 
@@ -13,3 +15,45 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# -----------------------------------------------------------------------------------------
+
+from django.contrib.auth.views import LoginView
+
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# -----------------------------------------------------------------------------------------
+
+from django.contrib.auth.views import LogoutView
+
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# -----------------------------------------------------------------------------------------
+# This can be used
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# This too
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class Register(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+
