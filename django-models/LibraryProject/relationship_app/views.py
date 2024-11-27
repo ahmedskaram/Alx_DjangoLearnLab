576ab47c8240785c2111ed96009d71904b6429f6
ahmedskaram <ahmedkaramak47@gmail.com>
2024-11-27 19:22:45 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -61,29 +61,26 @@ class Register(CreateView):
 # -------------------------------------------------------------------------------------------
 
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_tes
+from django.contrib.auth.decorators import user_passes_test
 
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-@user_passes_test(is_admin)
-def admin_view(request):
-    # This view is only accessible to users with the 'Admin' role
-    return render(request, 'relationship_app/admin_view.html')  # You can customize the template as needed
-
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    # This view is only accessible to users with the 'Librarian' role
-    return render(request, 'relationship_app/librarian_view.html') 
 
 def is_member(user):
     return user.userprofile.role == 'Member'
 
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
 @user_passes_test(is_member)
 def member_view(request):
-    # This view is only accessible to users with the 'Member' role
     return render(request, 'relationship_app/member_view.html')
 
