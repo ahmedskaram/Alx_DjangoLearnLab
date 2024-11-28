3e12fee590e9bb4f790d2d53663f0d0b26aa2caa
ahmedskaram <ahmedkaramak47@gmail.com>
2024-11-28 11:33:05 -0800

    Custom Permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -36,4 +36,15 @@ urlpatterns = [
 
 # -------------------------------------------------------------------------------------------
 
+# Custom Permissions
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add/', views.add_book, name='add_book'),
+    path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+]
+
+# -------------------------------------------------------------------------------------------
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -83,3 +83,42 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+# -------------------------------------------------------------------------------------------
+
+# Custom Permissions
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .models import Author
+
+# Add book View
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author_id = request.POST.get('author_id')
+        author = Author.objects.get(id=author_id)
+        new_book = Book.objects.create(title=title, author=author)
+        return redirect('list_books')
+    return render(request, 'relationship_app/add_book.html')
+
+# Change book View
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = Author.objects.get(id=request.POST.get('author_id'))
+        book.save()
+        return redirect('list_books')
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+# Delete book View
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    book.delete()
+    return redirect('list_books')
+
+# -------------------------------------------------------------------------------------------
