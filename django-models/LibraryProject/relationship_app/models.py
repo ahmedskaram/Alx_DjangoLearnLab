78f200c0351354ce9df9f91e09a39e3c9969d7e3
ahmedskaram <ahmedkaramak47@gmail.com>
2024-11-28 11:18:12 -0800

    role access 100

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -41,14 +41,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
-    # This function has the same role in the signals.py file.
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            UserProfile.objects.create(user=instance)
-
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-# -------------------------------------------------------------------------------------------
 
 # -------------------------------------------------------------------------------------------
--- django-models/LibraryProject/relationship_app/signals.py
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -8,4 +10,5 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save(
+    instance.userprofile.save()
+
