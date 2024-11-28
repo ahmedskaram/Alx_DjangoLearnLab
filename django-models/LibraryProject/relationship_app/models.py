bc2a4325776dd34288cb1d91219119e73eee8cda
ahmedskaram <ahmedkaramak47@gmail.com>
2024-11-28 12:04:14 -0800

    Custom Permissions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -10,8 +10,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
-        # Custom Permissions
-        class Meta:
+    # Custom Permissions
+    class Meta:
         permissions = [
             ("can_add_book", "Can add a book"),
             ("can_change_book", "Can change a book"),
