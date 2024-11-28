e2a7dd85b32adb220527d79612b31b8287f6eea2
ahmedskaram <ahmedkaramak47@gmail.com>
2024-11-28 12:00:04 -0800

    Custom Permissions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,7 +9,15 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    
+
+        # Custom Permissions
+        class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+
     def __str__(self):
         return self.title
     
@@ -44,24 +52,5 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-
-# -------------------------------------------------------------------------------------------
-
-# Custom Permissions
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add a book"),
-            ("can_change_book", "Can change a book"),
-            ("can_delete_book", "Can delete a book"),
-        ]
-
-    def __str__(self):
-        return self.title
 
 # -------------------------------------------------------------------------------------------
