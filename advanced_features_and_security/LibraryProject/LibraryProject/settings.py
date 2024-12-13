fb40f478f1eba5fefc1c27ae7c76a149e848aede
ahmedskaram <ahmedkaramak47@gmail.com>
2024-12-13 12:54:10 -0800

    Secure Redirects

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -154,6 +154,9 @@ X_FRAME_OPTIONS = "DENY"
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 
+# Proxy SSL Header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # -------------------------------------------------------------------------------------------
 
 
