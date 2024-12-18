a9d3352b957efb9494e2736d525f8175870d51e1
ahmedskaram <ahmedkaramak47@gmail.com>
2024-12-18 06:30:44 -0800

    ViewSets and Routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,3 +7,17 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+# ---------------------------------------------------------
+
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# ---------------------------------------------------------
+
+
+
