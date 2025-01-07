dbf2069305e6f649f49ad7df5e30decfc96f5f4b
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-07 03:43:43 -0800

    blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+# blog/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+
+# تسجيل المستخدم
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+# تسجيل الدخول
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to home page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+# تسجيل الخروج
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+# إدارة الملف الشخصي
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.email = request.POST['email']
+        user.save()
+        return redirect('profile')
+    return render(request, 'registration/profile.html', {'user': request.user}
