39062d64727485b2e4e6a91faeba0d61094da913
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-07 04:05:19 -0800

    blog

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -80,9 +80,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': 'test_db',  # Optional: Custom test database name
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
