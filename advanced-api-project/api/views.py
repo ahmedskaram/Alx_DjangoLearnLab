5db05976600aa0ef0cc0f4dde20da0373c88557a
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-07 02:51:01 -0800

    Hi

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from django_filters import rest_framework
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
