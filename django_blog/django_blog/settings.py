da9a6eb3783de40185c6395d958bee2661db3e5c
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-07 03:35:45 -0800

    blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoDB',
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST': 'localhost',
+        'PORT': '3308',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
