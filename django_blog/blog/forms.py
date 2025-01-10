ebd15ea0d1ed3382d9697caf54700a1824c4ce42
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-10 09:35:35 -0800

    Edit

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -26,9 +26,13 @@ class CommentForm(forms.ModelForm):
 
 from django import forms
 from .models import Post
+from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+                'tags': TagWidget()
+        }
 
