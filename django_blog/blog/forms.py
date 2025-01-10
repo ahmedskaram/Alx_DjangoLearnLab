18ab242322d1ff69828b503a3d8e1f92a9d8807c
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-10 10:40:06 -0800

    Edit

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -12,6 +12,19 @@ class CustomUserCreationForm(UserCreationForm):
 ##########################################################################################
 
 from django import forms
+from django.contrib.auth.models import User
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+##########################################################################################
+
+from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
