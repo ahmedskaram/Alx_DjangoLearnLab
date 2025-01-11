fc7d8a0f692bd15df00f3506997222306aeb0bd1
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-11 07:12:43 -0800

    push

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
-    #'posts',
+    'posts',
     'rest_framework.authtoken',
 ]
 
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoDB',
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST': 'localhost',
+        'PORT': '3308',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
