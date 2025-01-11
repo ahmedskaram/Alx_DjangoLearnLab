14811734061759387c247b701e2f7e147fafebe4
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-11 04:17:33 -0800

    push

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-from .models import CustomUser
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+
+User = get_user_model()  # Retrieve the User model being used
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  # Make the password field write-only
+
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture', 'followers']
+
+    def create(self, validated_data):
+        # Create a new user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        # Create a Token for the user
+        Token.objects.create(user=user)
+        return user
