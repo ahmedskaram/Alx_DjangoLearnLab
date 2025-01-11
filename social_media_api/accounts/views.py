12274c369ec2770d35353e10c314397c0d24820e
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-11 06:17:31 -0800

    edit

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -32,10 +32,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
-Customer  = get_user_model()
+CustomUser  = get_user_model()
 
 class UserListView(generics.GenericAPIView):
-    queryset = Customer.objects.all()
+    queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
