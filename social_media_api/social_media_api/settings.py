edd538d10e8de64f8bf7fac538cf7e540a0c32a4
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-11 07:10:00 -0800

    edit

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9)z4%5_&wu&9_#m0m6ivo0#*mcmjw^8#z1v&)%$r9(!w#%z9t*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com']
 
 
 # Application definition
@@ -138,3 +138,9 @@ REST_FRAMEWORK = {
 }
 
 ##########################################################################################
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+
