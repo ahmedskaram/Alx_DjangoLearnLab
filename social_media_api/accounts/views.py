c98a555510a45fb8700bf7c158a6e66f1586f1f9
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-11 06:20:11 -0800

    edit

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -36,7 +36,7 @@ CustomUser  = get_user_model()
 
 class UserListView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         users = self.get_queryset()
