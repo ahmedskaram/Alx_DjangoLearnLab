f9312184b4bf9fe62b9175a88448729f2a81543e
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-20 12:05:29 +0200

    edit

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    # Custom Permissions
+    # Still need code group permissions.
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+
+    def __str__(self):
+        return self.title
+    
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+    
+    def __str__(self):
+        return self.name
+    
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    
+    def __str__(self):
+        return self.name
+
+# -------------------------------------------------------------------------------------------
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+    # This function has the same role in the signals.py file.
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# -------------------------------------------------------------------------------------------
