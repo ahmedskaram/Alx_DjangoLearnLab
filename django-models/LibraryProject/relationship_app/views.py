acb518f32ac5bd35436be683c4f8e69eec71a7d7
ahmedskaram <ahmedkaramak47@gmail.com>
2025-01-20 11:54:10 +0200

    admin

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -64,26 +64,25 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
-@user_passes_test(is_admin)
+@user_passes_test(is_admin, login_url='/login/')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(is_librarian)
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian, login_url='/login/')
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
-@user_passes_test(is_member)
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_member, login_url='/login/')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
-
 # -------------------------------------------------------------------------------------------
 
 # Custom Permissions
@@ -109,7 +108,7 @@ def edit_book(request, book_id):
     book = Book.objects.get(id=book_id)
     if request.method == 'POST':
         book.title = request.POST.get('title')
-        book.author = Author.objects.get(id=request.POST.get('author_id'))
+        book.author = Author.objects.get(id=request.POST.get('author_id')):
         book.save()
         return redirect('list_books')
     return render(request, 'relationship_app/edit_book.html', {'book': book})
